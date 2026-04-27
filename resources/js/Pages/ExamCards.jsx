8b41b02ff4cd55a22aae80c6c5997c949b022a85
omekeuchechi <omekejoseph97@gmail>
2026-04-27 12:28:00 +0100

    let see if the identifyam issue is still showing

--- resources/js/Pages/ExamCards.jsx
+++ resources/js/Pages/ExamCards.jsx
@@ -216,7 +216,7 @@ export default function ExamCards({ auth }) {
                         <div className="logo-image">
                             <img src="/assets/svg/shield.svg" alt="" />
                         </div>
-                        <span>IDENTIFYAM</span>
+                        <span style={{ backgroundColor: 'red' }}>IDENTIFYAM</span>
                     </a>
 
                     <nav className="sidebar-menu">
